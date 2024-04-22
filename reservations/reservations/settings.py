d65c11dc9681481137d88f3ee79c2e9a1b3fcb59
Alexandre TONON <alexandre.tonon@student.isae-supaero.fr>
2024-04-22 16:26:04 +0200

    prémices du bootstrap

--- reservations/reservations/settings.py
+++ reservations/reservations/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    "django_bootstrap5"
+    "bootstrap5",
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
