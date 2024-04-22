e8b2f18f79bbbe8e29abb360f6ed86a621fffcd9
Alexandre TONON <alexandre.tonon@student.isae-supaero.fr>
2024-04-22 15:09:59 +0200

    implementation progressive de l'authentofcaiton : premier test ok

--- reservations/reservations/urls.py
+++ reservations/reservations/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin, auth
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("booking/", include("booking.urls"))
+    path("booking/", include("booking.urls")),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
