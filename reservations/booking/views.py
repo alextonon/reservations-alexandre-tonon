4cd27e64edcc1c63c0b34711374ef7c6eb1badb1
Alexandre TONON <alexandre.tonon@student.isae-supaero.fr>
2024-04-22 16:08:16 +0200

    ajout decorateur

--- reservations/booking/views.py
+++ reservations/booking/views.py
@@ -3,14 +3,18 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Trajet
 from .models import Reservation
 
+from django.contrib.auth.decorators import login_required
+
 def trajets(request):
     trajets = get_list_or_404(Trajet.objects.all())
     return render(request, 'booking/trajets.html', {'trajets': trajets})
 
+@login_required
 def reservations(request):
     reservations = get_list_or_404(Reservation, client=request.user)
     return render(request, 'booking/reservations.html', {'reservations': reservations})
 
+@login_required
 def reservation_details(request, numero_reservation):
     reservation = get_object_or_404(Reservation, pk=numero_reservation)
     return render(request, 'booking/reservation.html', {'reservation': reservation})
