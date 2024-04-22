c5b3134033dff0fbb7c0dc3608ded504cb428fe8
Alexandre TONON <alexandre.tonon@student.isae-supaero.fr>
2024-04-22 14:36:21 +0200

    gare et trajets ok avec admin et en + views trajets ok

--- reservations/booking/views.py
+++ reservations/booking/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 
-# Create your views here.
+from .models import Trajet
+
+def trajets(request):
+    trajets = get_list_or_404(Trajet.objects.all())
+    return render(request, 'booking/trajets.html', {'trajets': trajets})
