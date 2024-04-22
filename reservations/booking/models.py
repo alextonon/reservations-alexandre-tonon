417bc6417755035fd810879fdef690b1313e0ed1
Alexandre TONON <alexandre.tonon@student.isae-supaero.fr>
2024-04-22 14:16:13 +0200

    def classes gare et trajet

--- reservations/booking/models.py
+++ reservations/booking/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from datetime import datetime
 
 class Gare(models.Model):
     nom = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nom
+
+class Trajet(models.Model):
+    gare_depart = models.ForeignKey(Gare, on_delete=models.CASCADE, related_name='gare_depart')
+    gare_arrivee = models.ForeignKey(Gare, on_delete=models.CASCADE, related_name='gare_arrivee')
+    heure_depart = models.DateTimeField()
+    heure_arrivee = models.DateTimeField()
+    
+
+    def __str__(self):
+        return f'{self.depart} -> {self.arrivee}'
